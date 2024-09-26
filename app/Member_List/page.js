2a6060a43df586468e4e249f00a79d37081f606a
AshokaYadav <yashoka5111718258@gmail.com>
2024-09-26 10:05:50 +0530

    change for comming three

--- app/Member_List/page.js
+++ app/Member_List/page.js
@@ -2,7 +2,7 @@ import MemberList from '@/components/MemberList/MemberList'
 import React from 'react'
 
 const page = () => {
-  //change for comming two
+  //change for comming three
   return (
    <>
    <MemberList/>
