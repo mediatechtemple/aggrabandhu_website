902a27a3227efc770539303eea6cc2906e164ca4
Ajeet Kumar Jha <84560142+Ajeetjha123@users.noreply.github.com>
2024-10-16 05:43:29 -0700

    add policy responsive code

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -2,13 +2,12 @@
 import React, { useEffect, useState } from "react";
 
 const PrivatePolicy = () => {
-  const [privacyPolicy, setPrivacyPolicy] = useState([]);
-  const handelPrivatePolicy = async () => {
+  const [privacyPolicy, setPrivacyPolicy] = useState("");
+
+  const handlePrivatePolicy = async () => {
     try {
       const data = await fetch("https://backend.aggrabandhuss.org/api/policy");
-
       const result = await data.json();
-
       setPrivacyPolicy(result[0].policy);
     } catch (error) {
       console.log(error);
@@ -16,11 +15,15 @@ const PrivatePolicy = () => {
   };
 
   useEffect(() => {
-    handelPrivatePolicy();
+    handlePrivatePolicy();
   }, []);
+
   return (
-    <div className="mt-20 max-w-4xl mx-auto">
-      <div dangerouslySetInnerHTML={{ __html: privacyPolicy }} />
+    <div className="mt-20 mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+      <div
+        className="prose prose-lg md:prose-xl mx-auto max-w-full overflow-hidden"
+        dangerouslySetInnerHTML={{ __html: privacyPolicy }}
+      />
     </div>
   );
 };
