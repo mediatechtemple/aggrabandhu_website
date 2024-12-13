d039253928358a15555f0570a6cb6f179a7c8638
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2024-12-13 15:50:59 +0530

    add Link in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,15 +8,20 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const Footer = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="bg-black w-full py-8">
       <div className="flex flex-col md:flex-row justify-around items-center gap-8">
         <div className="flex flex-col md:flex-row items-center gap-6 text-lg font-semibold text-white uppercase">
-        <Link href="https://wa.me/917830305040" target="blank" >Support</Link>
+          <Link href="https://wa.me/917830305040" target="blank">
+            Support
+          </Link>
           <Link href={"privacy"}>Privacy Policy</Link>
           <Link href={"rule"}>Rules & Regulations</Link>
-          <Button onClick={() => router.push('contact-us')}  className="rounded-full font-semibold uppercase bg-orange-600 border border-green-500 hover:bg-orange-700">
+          <Button
+            onClick={() => router.push("contact-us")}
+            className="rounded-full font-semibold uppercase bg-orange-600 border border-green-500 hover:bg-orange-700"
+          >
             Contact Us
           </Button>
         </div>
@@ -25,8 +30,22 @@ const Footer = () => {
       <hr className="border-t border-gray-400 my-4" />
 
       <p className="text-white text-center text-sm uppercase">
-        All Rights Reserved to Agrabandhu Sewarth Sansthan (ABSS) - Designed &
-        Developed by MediaTechTemple
+        All Rights Reserved to Agrabandhu Sewarth Sansthan (ABSS) |
+        <Link
+          href="https://www.mediatechtemple.com/web-design-and-development-services/"
+          target="_blank"
+          title="Media Tech Temple"
+        >
+          &nbsp; Website Developed By
+        </Link>
+        <Link
+          href="https://mediatechtemple.com/"
+          target="_blank"
+          title="Media Tech Temple"
+        >
+          {" "}
+          Media Tech Temple
+        </Link>
       </p>
 
       <div className="fixed bottom-2 right-8 z-20 flex flex-col space-y-2">
